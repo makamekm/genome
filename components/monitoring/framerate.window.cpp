f1b61fd1657e92b2fd77fa65d8e3fec535e6ef36
makame <makame@mail.ru>
2020-05-25 17:48:45 +0200

    add style

--- components/monitoring/framerate.window.cpp
+++ components/monitoring/framerate.window.cpp
@@ -3,22 +3,22 @@
 #include "../plot-var/plot-var.h"
 
 namespace Monitoring {
-	void RenderFramerateWindow(const int &frameRate, const double &frameTime)
+	void RenderFramerateWindow(const int &frameRate, const double &frameTime, const char* name)
 	{
-		ImGui::SetNextWindowPos(ImVec2(10,10), ImGuiCond_Appearing );
+		ImGui::SetNextWindowPos(ImVec2(10,10), ImGuiCond_FirstUseEver );
 		ImGui::SetNextWindowSize(ImVec2(300,0), ImGuiCond_Always );
-		ImGui::Begin("Monitoring::Framerate");
+		ImGui::Begin(name);
 		ImGui::PlotVar("FPS", frameRate, 0, 60);
 		ImGui::PlotVar("Frame Time", frameTime, 0);
 		ImGui::End();
 	}
 
-	void RenderTimelineWindow()
+	void RenderTimelineWindow(const char* name)
 	{
 		double currentTime = glfwGetTime();
-		ImGui::SetNextWindowPos(ImVec2(10,135), ImGuiCond_Appearing );
+		ImGui::SetNextWindowPos(ImVec2(10,135), ImGuiCond_FirstUseEver );
 		ImGui::SetNextWindowSize(ImVec2(300,0), ImGuiCond_Always );
-		ImGui::Begin("Monitoring::Timeline");
+		ImGui::Begin(name);
 		ImGui::Text("%s: %-3.4f", "Global Time", currentTime);
 		ImGui::End();
 	}
